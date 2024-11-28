44fdf1c4b37e8b5e9d213066b65e1e52fd19a73e
PratimaP23 <156713745+PratimaP23@users.noreply.github.com>
2024-11-28 23:14:30 +0530

    Update Smallest_Postive_Missing.java

--- Array/Smallest_Postive_Missing.java
+++ Array/Smallest_Postive_Missing.java
@@ -1,10 +1,26 @@
-package Array;
-
-public class Smallest_Postive_Missing {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) {
+        // Your code here
+       int n=arr.length;
+       Arrays.sort(arr);
+       int num=1;
+       
+       for(int i=0;i<n;i++){
+           
+           if(arr[i]==num && arr[i]>=1){
+               
+               num++;
+               
+           }else if(arr[i]>num){
+               
+               break;
+               
+           }
+          
+       }
+       
+       return num;
+      
+    } 
 }
